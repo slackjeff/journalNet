e1d40e320ab062d6806aa68075d80a6d4b0d9463
Demogorgoff <155981302+Demogorgoff@users.noreply.github.com>
2024-07-23 10:21:57 -0300

    New wizard banner.
    New wizard banner.

--- banner/wizard_banner.sh
+++ banner/wizard_banner.sh
@@ -1,18 +1,24 @@
-#!/usr/bin/env bash
-echo -e '\033[0m'
-echo -e '\033[1;37m*                                     '
-echo -e ' \033[1;33m(\.          \033[1;33m./)                     ' 
-echo -e '  \(   \033[1;94m|\\     \033[1;33m)/                      '
-echo -e '  \033[1;94m//\  | \   /\\'\\'                      '
-echo -e ' \033[1;94m(/ /\_\033[1;37m#\033[1;33moo\033[1;37m#\033[1;94m_/\ \)                     '
-echo -e '  \033[1;94m\/\_ \033[1;37m#### \033[1;94m_/\/                      '
-echo -e '    \033[1;37m_  \033[1;94m\\\033[1;37m##\033[1;94m/ \033[1;37m_          _         _    '         
-echo -e ' __/\\'\\'__  _/\\'\\'___   __/\\'\\'___  __/\\'\\'__ ' 
-echo -e '(_    _))(_      ))(_  ____))(__  __))' 
-echo -e '  \  \\'\\'   /  :   \\'\\' /  ._))    /  \\'\\'  ' 
-echo -e '\033[1;90m/\/ \033[1;37m.:\033[1;90m\\'\\' /\033[1;37m:.\033[1;90m |   ///\033[1;37m:.\033[1;90m ||___  /\033[1;37m:.\033[1;90m  \\'\\' ' 
-echo -e '\__  _// \___|  // \  _____)) \__  // ' 
-echo -e '   \//        \//   \//          \//  ' 
-echo -e '\033[0m'                                                                                      
-#Wizard by Ojo.
+#!/bin/sh
 
+RESET='\033[0m'
+BOLD_WHITE='\033[1;37m'
+BOLD_YELLOW='\033[1;33m'
+BOLD_BLUE='\033[1;94m'
+BOLD_GRAY='\033[1;90m'
+
+printf "${RESET}\n"
+printf "${BOLD_WHITE}*                                     \n"
+printf "${BOLD_YELLOW} (\.          ./)                   \n"  
+printf "${BOLD_YELLOW}  \(   ${BOLD_BLUE}|\     ${BOLD_YELLOW})/                      \n"
+printf "${BOLD_BLUE}  //\  | \   /\%s\                      \n"
+printf "${BOLD_BLUE} (/ /\_${BOLD_WHITE}#${BOLD_YELLOW}oo${BOLD_WHITE}#${BOLD_BLUE}_/\ \)                     \n"
+printf "${BOLD_BLUE}  \/\_ ${BOLD_WHITE}#### ${BOLD_BLUE}_/\/                      \n"
+printf "${BOLD_WHITE}    _  ${BOLD_BLUE}\%s${BOLD_WHITE}##${BOLD_BLUE}/ ${BOLD_WHITE}_          _         _    \n"
+printf "${BOLD_WHITE} __/\%s\__  _/\%s\___   __/\%s\___  __/\%s\__ \n"
+printf "${BOLD_WHITE}(_    _))(_      ))(_  ____))(__  __))\n"
+printf "${BOLD_WHITE}  \  \%s\   /  :   \%s\ /  ._))    /  \%s\  \n"
+printf "${BOLD_GRAY}/\/ ${BOLD_WHITE}.:${BOLD_GRAY}\%s\ /${BOLD_WHITE}:. ${BOLD_GRAY}|   ///${BOLD_WHITE}:. ${BOLD_GRAY}||___  /${BOLD_WHITE}:.${BOLD_GRAY}  \%s\ \n"
+printf "${BOLD_GRAY}\__  _// \___|  // \  _____)) \__  // \n"
+printf "${BOLD_GRAY}   \//        \//   \//          \//  \n"
+printf "${RESET}\n"                                                                            
+#Wizard by Ojo.
